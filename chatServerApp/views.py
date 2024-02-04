a900ecaf143b69cc00cf028ec94dd1dabfe36f8d
wajihMahrouseh <wajiho.mahrouseh@gmail.com>
2024-02-04 23:27:02 +0300

    add the room view

--- chatServerApp/views.py
+++ chatServerApp/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "chatServerApp/index.html")
+
+
+def room(request, room_name):
+    return render(request, "chatServerApp/room.html", {"room_name": room_name})
